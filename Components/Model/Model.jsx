d769dc07f9718acab94a6823da55b00bde8c9dc0
cgrade <gradeisageek@gmail.com>
2024-02-17 00:25:34 +0100

    Model Components and styling added

--- Components/Model/Model.jsx
+++ Components/Model/Model.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image';
 
-const Model = () => {
+// INTERNAL IMPORT
+import Style from './Model.module.css';
+import images from './../../assets';
+
+const Model = ({setOpenModel, connectWallet }) => {
+
+  // Use State
+  const walletMenu = ["MetaMask", "Coinbase", "Wallet", "WalletConnect"]
   return (
-    <div>Model</div>
+    <div className={Style.Model}>
+      <div className={Style.Model_box}>
+        <div className={Style.Model_box_heading}>
+          <p>Connect a Wallet</p>
+          <div className={Style.Model_box_heading_img}>
+            <Image src={images.close}
+            alt='logo'
+            width={50}
+            height={50}
+            onClick={() => setOpenModel(false)} />
+          </div>
+        </div>
+
+
+        <div className={Style.Model_box_wallet}>
+          {walletMenu.map((item, i) =>(
+            <p key={i + 1} onClick={() => connectWallet()}>
+              {item}
+            </p>
+          ))}
+        </div>
+
+        <p className={Style.Model_box_para}>
+          By connecting a wallet, you agree to Uniswap Labs' <br />Terms and
+          Service and consent to its Privacy Policy.
+        </p>
+      </div>
+
+    </div>
   )
 }
 
