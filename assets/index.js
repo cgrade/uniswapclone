b2585fdb0ea662bc4ffc57ef02615ad934feebc6
cgrade <gradeisageek@gmail.com>
2024-02-19 13:48:28 +0100

    all the assests for the uniswap ui

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+import arrowup from './arrowUp.png';
+import close from './close.png';
+import ether from './ether.png';
+import etherlogo from './etherlogo.png';
+import homeicon from './home.png';
+import lock from './lock.png';
+import arrow from './arrowUp.png';
+import uniswap from './logo.png';
+import search from './search.png';
+
+export default {
+    uniswap,
+    search,
+    ether,
+    arrow,
+    arrowup,
+    lock,
+    homeicon,
+    etherlogo,
+    close,
+}
